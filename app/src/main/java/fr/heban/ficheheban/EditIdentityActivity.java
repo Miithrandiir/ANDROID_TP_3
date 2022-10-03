df8efb553cc5fea917a884993493ecc29da51cce
Simon <simon.heban@ferenost.fr>
2022-10-03 21:27:36 +0200

    feat: edit identity activity

---
+++ app/src/main/java/fr/heban/ficheheban/EditIdentityActivity.java
@@ -0,0 +1,14 @@
+package fr.heban.ficheheban;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditIdentityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_identity);
+    }
+}
